3bf24806ef49907916aea46f7a777dda05a20b0f
skkad <skkad1998@gmail.com>
2022-08-28 21:57:19 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -3,22 +3,26 @@ const fs = require('fs/promises')
 const myFileWriter = async (fileName, fileContent) => {
 	// write code here
 	// dont chnage function name
+	fs.writeFile(fileName,fileContent);
 }
 
 const myFileReader = async (fileName) => {
 	// write code here
 	// dont chnage function name
+	fs.readFile(fileName);
 }
 
 
 const myFileUpdater = async (fileName, fileContent) => {
 	// write code here
 	// dont chnage function name
+	fs.appendFile(fileName,fileContent);
 }
 
 const myFileDeleter = async (fileName) => {
 	// write code here
 	// dont chnage function name
+	fs.unlink();
 }
 
 
